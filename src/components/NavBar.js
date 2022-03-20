827d9e50d38a388f14cc36be9581c0bca8763dd2
Sabrina Szpayzer <sabrinaszpayzer@yahoo.com.ar>
2022-03-20 12:59:30 -0300

    CartWidget & ItemListContainer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import CartWidget from './CartWidget';
 
 function NavBar () {
     return(
         <header>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">Luna | Aromas y Velas</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -22,6 +23,7 @@ function NavBar () {
                             </li>
                         </ul>
                     </div>
+                    <CartWidget />
                 </div>
             </nav>
         </header>
